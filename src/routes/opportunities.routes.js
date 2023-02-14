9009b28cbaadde7fa44034d024f285e812cc040e
Gustavo G Passos <girardi_gustavo@outlook.com>
2023-02-13 23:13:10 -0300

    ajustes nas chamadas e criação de novas rotas

--- src/routes/opportunities.routes.js
+++ src/routes/opportunities.routes.js
@@ -4,9 +4,10 @@ const opportunitiesRouter = Router()
 
 const controller = require('./../controllers/opportunities')
 
-opportunitiesRouter.get('/', controller.findAll)
-opportunitiesRouter.get('/:id', controller.findById)
+opportunitiesRouter.get('/', controller.getAll)
+opportunitiesRouter.get('/:id', controller.getById)
 opportunitiesRouter.post('/create', controller.create)
 opportunitiesRouter.patch('/:id', controller.update)
+opportunitiesRouter.get('/company/:id', controller.getByCompanyId)
 
 module.exports = opportunitiesRouter
